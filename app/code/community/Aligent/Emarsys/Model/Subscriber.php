6e34b8b9cf3c1df87b67ef9c541841cab2d35adb
Kath Young <kath.young@aligent.com.au>
2017-10-09 08:44:19 +1030

    ALG-76 Added automatic email suppression where Emarsys subscription is enabled.

---
+++ app/code/community/Aligent/Emarsys/Model/Subscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+class Aligent_Emarsys_Model_Subscriber extends Mage_Newsletter_Model_Subscriber {
+    protected $_helper = null;
+
+    public function sendConfirmationSuccessEmail() {
+        if( ! $this->helper()->isSubscriptionEnabled()) {
+            return parent::sendConfirmationSuccessEmail();
+        }
+        return $this;
+    }
+
+    public function sendUnsubscriptionEmail(){
+        if(!$this->helper()->isSubscriptionEnabled()){
+            return parent::sendUnsubscriptionEmail();
+        }
+        return $this;
+    }
+
+    protected function helper(){
+        if($this->_helper == null){
+            $this->_helper = Mage::helper('aligent_emarsys');
+        }
+        return $this->_helper;
+    }
+}
