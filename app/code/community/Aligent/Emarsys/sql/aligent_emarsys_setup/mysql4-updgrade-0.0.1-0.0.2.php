71e1d1f27630c2615e1060df35d608dc41e001b9
Kath Young <kath.young@aligent.com.au>
2017-10-27 11:42:50 +1030

    SWG-275 - Added additional checking to avoid inadvertent duplicates, more indexing for performance and additional logging.

---
+++ app/code/community/Aligent/Emarsys/sql/aligent_emarsys_setup/mysql4-updgrade-0.0.1-0.0.2.php
@@ -0,0 +1,57 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+
+$tableName = $installer->getTable('aligent_emarsys/remoteSystemSyncFlags');
+// Check if the table already exists
+if ($installer->getConnection()->isTableExists($tableName)) {
+    // Remove any of the duplicate records that are going to break the scripts
+    $Items = Mage::getModel('aligent_emarsys/remoteSystemSyncFlags')->getCollection();
+    $Items->removeAllFieldsFromSelect();
+    $Items->getSelect()->columns('max(id) as mId')->group('customer_entity_id')->having('count(id) > 1')->where('customer_entity_id > 0');
+
+    $Ids = [];
+    foreach($Items as $Item){
+        $Ids[] = $Item->getData('mId');
+    }
+
+    $Items = Mage::getModel('aligent_emarsys/remoteSystemSyncFlags')->getCollection()->addFieldToFilter('id', ['in'=>$Ids]);
+    foreach($Items as $Item){
+        $Item->delete();
+    }
+    $table = $installer->getConnection();
+
+    // Still not making the indices unique as we need to have multiple customer_entity_id of zero
+    $table->addIndex(
+        $installer->getIdxName(
+            'aligent_emarsys/remoteSystemSyncFlags',
+            array('customer_entity_id'),
+            Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX
+        ),
+        array( 'customer_entity_id' ),
+        array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX)
+    );
+
+    $table->addIndex(
+        $installer->getIdxName(
+            'aligent_emarsys/remoteSystemSyncFlags',
+            array('newsletter_subscriber_id'),
+            Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX
+        ),
+        array( 'customer_entity_id' ),
+        array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX)
+    );
+
+    $table->addIndex(
+        $installer->getIdxName(
+            'aligent_emarsys/remoteSystemSyncFlags',
+            array('email'),
+            Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX
+        ),
+        array( 'email' ),
+        array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX)
+    );
+
+    $installer->endSetup();
+}
