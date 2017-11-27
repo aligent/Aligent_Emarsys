d9ab794edd3d96de5e62c7185ad1cb7495a98a97
Kath Young <kath.young@aligent.com.au>
2017-11-27 10:56:13 +1030

    SWG-275 - Added creation and update timestamps.  Setup script to actually remove invalid duplicates.  Added parameter required for consolidated product feeds.

--- app/code/community/Aligent/Emarsys/Model/RemoteSystemSyncFlags.php
+++ app/code/community/Aligent/Emarsys/Model/RemoteSystemSyncFlags.php
@@ -6,4 +6,14 @@ class Aligent_Emarsys_Model_RemoteSystemSyncFlags extends Mage_Core_Model_Abstra
     {
         $this->_init('aligent_emarsys/remoteSystemSyncFlags');
     }
+
+    protected function _beforeSave()
+    {
+        parent::_beforeSave();
+        if($this->isObjectNew()){
+            $this->setCreatedAt( Mage::getModel('core/date')->date('Y-m-d H:i:s') );
+        }
+        $this->setUpdatedAt( Mage::getModel('core/date')->date('Y-m-d H:i:s') );
+        return $this;
+    }
 }
