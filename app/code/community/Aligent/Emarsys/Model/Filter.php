1b327827867061cd8146718111b15a6179198d7f
Kath Young <kath.young@aligent.com.au>
2018-01-12 14:29:04 +1030

    BEN-198 : Removed redundant feed, adjusted names.  Ensure getting stock from EAV tables, as flat catalog doesn't include disabled products.

--- app/code/community/Aligent/Emarsys/Model/Filter.php
+++ app/code/community/Aligent/Emarsys/Model/Filter.php
@@ -5,65 +5,57 @@
  */
 class Aligent_Emarsys_Model_Filter {
     /**
-     * This before query filter will:
-     *  - Join the stock_status table so that we can access the qty and availability
-     *  - Prevent Simple items from being output - these will be handled by exportSimpleChildren
-     *
-     * Also note as this uses stock, we will need to ensure that the reindex before feed generation option is switched on
-     *
-     * @param Varien_Db_Select $oSelect The current select query
-     * @param Mage_Core_Model_Store $oStore Sore for which this feed is being generated
-     * @param array $aParams An array of parameters defined in the config.xml
+     * @param Varien_Db_Select $oSelect
+     * @param $oStore Mage_Core_Model_Store
+     * @param $aParms
      */
-    public function beforeQueryFilter(Varien_Db_Select &$oSelect, $oStore, $aParms) {
+    public function beforeQueryFilter(Varien_Db_Select &$oSelect, $oStore, $aParms)
+    {
+        Mage::app()->getStore()->setConfig('catalog/frontend/flat_catalog_product', 0);
         /** @var Aligent_Emarsys_Helper_Data $helper */
         $helper = Mage::helper('aligent_emarsys');
 
+        $websiteIds = array($oStore->getWebsiteId());
+        $storeId = $oStore->getStoreId();
+
+        $attrs = array('url_key','url_path','name','image_label','small_image','small_image_label','category_id','price','availability','brand_value');
+
+
+        $oSelect = Mage::getModel('catalog/product')->getCollection();
+
+        foreach($attrs as $attr){
+            $oSelect->addAttributeToSelect($attr);
+            $oSelect->addAttributeToSort($attr);
+        }
+        $oSelect->addStoreFilter($storeId);
+        $oSelect->addWebsiteFilter($websiteIds);
+
         $vStockStatus = Mage::getModel('core/resource_setup', 'core_setup')->getTable('cataloginventory/stock_status');
+        $vSuperLink = Mage::getModel('core/resource_setup', 'core_setup')->getTable('catalog/product_super_link');
 
-        $stockFromSimple = $helper->getGetStockFromSimpleProduct();
-        $includeSimpleParents = $helper->getIncludeSimpleParents();
-        $includeDisabled = $helper->getIncludeDisabled();
+        $oSelect = $oSelect->getSelect();
+        $oSelect->joinLeft( array( 'sl' => $vSuperLink ), '`e`.`entity_id`=`sl`.`product_id`');
+        $oSelect->joinLeft( array( 'pss' => $vStockStatus ),
+            '`sl`.parent_id=`pss`.`product_id` AND `pss`.website_id = ' . $oStore->getWebsiteId(),
+            array(
+                'ps_stock_qty' => 'sum(`pss`.`qty`)',
+                'ps_availability' => 'sum(`pss`.`stock_status`)'
+            )
+        );
 
-        if ($stockFromSimple) {
-            // Grab the super link and join on that.
-            $vSuperLink = Mage::getModel('core/resource_setup', 'core_setup')->getTable('catalog/product_super_link');
-            $oSelect->joinLeft( array( 'sl' => $vSuperLink ), '`main_table`.`entity_id`=`sl`.`parent_id`')
-                ->joinLeft( array( 'ss' => $vStockStatus ),
-                '`main_table`.`entity_id`=`ss`.`product_id`',
+        $oSelect->joinLeft( array( 'ss' => $vStockStatus ),
+                '`e`.`entity_id`=`ss`.`product_id` AND `ss`.website_id = ' . $oStore->getWebsiteId(),
                 array(
                     'stock_qty' => 'sum(`ss`.`qty`)',
                     'availability' => 'sum(`ss`.`stock_status`)'
                 )
-            )->group('COALESCE(`sl`.`parent_id`, `main_table`.`entity_id`)');
-        } else {
-            $oSelect->joinLeft( array( 'ss' => $vStockStatus ),
-                'main_table.entity_id=ss.product_id',
-                array(
-                    'stock_qty' => 'ss.qty',
-                    'availability' => 'ss.stock_status'
-                )
-            );
-        }
+            )->group('COALESCE(`sl`.`parent_id`, `e`.`entity_id`)');
 
-        if(!isset($aParms['include_all_stores'])){
-            $oSelect->where('ss.website_id=?', array($oStore->getWebsiteId()));
-        }
 
-        if ($includeSimpleParents) {
-            // Do not include simples, unless they have no parent.
-            $oSelect->where('`main_table`.`type_id` <> ? OR (`main_table`.`type_id` = ? AND `sl`.`parent_id` IS NULL)', 'simple');
-        } else {
-            $oSelect->where('`main_table`.`type_id` <> ?', 'simple');
-        }
-
-        if (!$includeDisabled) {
-            $oSelect->where('`main_table`.`status` = ?', Mage_Catalog_Model_Product_Status::STATUS_ENABLED);
-        }
+        // Do not include simples, unless they have no parent.
+        $oSelect->where('`e`.`type_id` <> ? OR (`e`.`type_id` = ? AND `sl`.`parent_id` IS NULL)', 'simple');
 
         $vSql = (string) $oSelect;
-
         Mage::getSingleton('aligent_feeds/log')->log("Catalog Select is: $vSql");
     }
-
 }
