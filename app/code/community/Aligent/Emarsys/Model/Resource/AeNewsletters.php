33e109a2ffb6456f46e424ad4bb5dfc6414b0c7e
Kath Young <kath.young@aligent.com.au>
2018-02-21 08:50:48 +1030

    Correct key field specification for AeNewsletters.

--- app/code/community/Aligent/Emarsys/Model/Resource/AeNewsletters.php
+++ app/code/community/Aligent/Emarsys/Model/Resource/AeNewsletters.php
@@ -4,6 +4,6 @@ class Aligent_Emarsys_Model_Resource_AeNewsletters extends Mage_Core_Model_Resou
 {
     protected function _construct()
     {
-        $this->_init('aligent_emarsys/aeNewsletters', 'id');
+        $this->_init('aligent_emarsys/aeNewsletters', 'subscriber_id');
     }
 }
