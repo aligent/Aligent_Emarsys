8e3778a91955c7b2656d2295f09f3ffab68d860c
Kath Young <kath.young@aligent.com.au>
2018-02-23 14:19:53 +1030

    Adjust availability to that disabled parents are never shown even if all children are enabled, and that enabled parents are never shown if all children are disabled.

--- app/code/community/Aligent/Emarsys/Model/Translator/Availability.php
+++ app/code/community/Aligent/Emarsys/Model/Translator/Availability.php
@@ -2,8 +2,11 @@
 class Aligent_Emarsys_Model_Translator_Availability
 {
     public function translate($aRow) {
-        $fieldName = ($aRow['type_id']=='simple') ? 'availability' : 'ps_availability';
-        $avail = isset($aRow[$fieldName]) ? $aRow[$fieldName] : false;
+        $avail = isset($aRow['availability']) ? $aRow['availability'] : false;
+        $parentAvail = isset($aRow['ps_availability']) ? $aRow['ps_availability'] : false;
+
+        if($aRow['type_id']!=='simple' && !$parentAvail) $avail = false;
+
         return $avail ? 'TRUE' : 'FALSE';
     }
 }
