6ebda9c95542aa239152e8830e1ce2dd40144175
Kath Young <kath.young@aligent.com.au>
2018-05-23 08:45:38 +0930

    Ensure availability checks both stock levels and "Enabled" status

--- app/code/community/Aligent/Emarsys/Model/Translator/Availability.php
+++ app/code/community/Aligent/Emarsys/Model/Translator/Availability.php
@@ -5,7 +5,13 @@ class Aligent_Emarsys_Model_Translator_Availability
         $avail = isset($aRow['availability']) ? $aRow['availability'] : false;
         $parentAvail = isset($aRow['ps_availability']) ? $aRow['ps_availability'] : false;
 
+        $stock = isset($aRow['stock_qty']) ? $aRow['stock_qty'] : false;
+        $parentStock = isset($aRow['ps_stock_qty']) ? $aRow['ps_stock_qty'] : false;
+
+        $stock = ($aRow['type_id']!=='simple') ? $stock : $parentStock;
         $avail = ($aRow['type_id']!=='simple') ? $avail : $parentAvail;
+
+        if($avail && $stock==0) $avail=false;
 
         return $avail ? 'TRUE' : 'FALSE';
     }
