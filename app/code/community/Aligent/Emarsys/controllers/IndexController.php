2c4595e6cb2777df6f63a5fd9d969ab877f9a899
Kath Young <kath.young@aligent.com.au>
2018-06-12 09:08:23 +0930

    Don't start sessions unnecessarily

--- app/code/community/Aligent/Emarsys/controllers/IndexController.php
+++ app/code/community/Aligent/Emarsys/controllers/IndexController.php
@@ -4,6 +4,16 @@ class Aligent_Emarsys_IndexController extends Mage_Core_Controller_Front_Action 
     /** @var Aligent_Emarsys_Helper_Emarsys */
     protected $_emHelper = null;
 
+    public function preDispatch()
+    {
+        $this->getLayout()->setArea($this->_currentArea);
+        $this->setFlag('', self::FLAG_NO_START_SESSION, 1); // Do not start standard session
+        $this->setFlag('', self::FLAG_NO_PRE_DISPATCH, 1);
+        $this->setFlag('', self::FLAG_NO_POST_DISPATCH, 1);
+
+        return parent::preDispatch();
+    }
+
     /**
      * @return Aligent_Emarsys_Helper_Emarsys
      */
@@ -21,6 +31,8 @@ class Aligent_Emarsys_IndexController extends Mage_Core_Controller_Front_Action 
     public function cookieupdateAction() {
         $oResponse = $this->getResponse();
         $oResponse->setBody('{"failure": true}');
+
+        $this->setFlag('',self::FLAG_NO_START_SESSION, true);
 
         if(Mage::helper('aligent_emarsys')->isEnabled()) {
             Mage::helper('aligent_emarsys')->updateCookieFromQuote();
