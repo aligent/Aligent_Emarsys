e3a9dbd8dd56c565727c686b0238906714fe1f4f
Kath Young <kath.young@aligent.com.au>
2018-10-02 08:37:58 +0930

    Added PHPdocs, corrected incorrect connect method

--- app/code/community/Aligent/Emarsys/Model/HarmonyFTPClient.php
+++ app/code/community/Aligent/Emarsys/Model/HarmonyFTPClient.php
@@ -4,23 +4,23 @@ use FtpClient\FtpClient;
 class Aligent_Emarsys_Model_HarmonyFTPClient {
     /** @var Aligent_Emarsys_Helper_Data */
     protected $_helper = null;
-
+    /** @var string $_host */
     protected $_host;
-
+    /** @var string $_port */
     protected $_port;
-
+    /** @var string $_user */
     protected $_user;
-
+    /** @var string $_pass */
     protected $_pass;
-
+    /** @var string $_exportDir */
     protected $_exportDir;
-
+    /** @var bool $_pasv */
     protected $_pasv;
-
+    /** @var bool $_sFTP */
     protected $_sFTP;
-
+    /** @var Varien_Io_Sftp|FtpClient $_actualClient  */
     protected $_actualClient = null;
-
+    /** @var int $_timeout */
     protected $_timeout = 15;
 
     public function __construct(){
@@ -34,25 +34,45 @@ class Aligent_Emarsys_Model_HarmonyFTPClient {
         $this->_exportDir = $this->_helper->getHarmonyFTPExportDir();
     }
 
+    /**
+     * Connects and logs in to the FTP/sFTP server (settings dependant).
+     */
     public function connect(){
         if(!$this->_actualClient) $this->_createClient();
+        $args = array(
+            'host' => $this->_host,
+            'username' => $this->_user,
+            'password' => $this->_pass,
+            'passive' => $this->_pasv
+        );
         if($this->_sFTP){
-            $this->_actualClient->connect($this->_host, false, $this->_port, $this->_timeout);
-            $this->_actualClient->login($this->_user, $this->_pass);
+            return $this->_actualClient->open($args);
         }else{
-            $this->_actualClient->connect(array(
-                'host' => $this->_host,
-                'username' => $this->_user,
-                'password' => $this->_pass,
-                'passive' => $this->_pasv
-            ));
+            try{
+                $this->_actualClient->connect($args);
+            }catch(\FtpClient\FtpException $e){
+                return false;
+            }
         }
     }
 
+    /**
+     * Changes the current directory on the remote
+     * @param $dir
+     * @return mixed
+     */
     public function chdir($dir){
         return $this->_sFTP ? $this->_actualClient->cd($dir) : $this->_actualClient->chdir($dir);
     }
 
+    /**
+     * Creates the remote file $fileName from the contents of
+     * $fileContents
+     *
+     * @param $fileName
+     * @param $fileContents
+     * @return mixed
+     */
     public function putFromString($fileName, $fileContents){
         if($this->_sFTP){
             $tmpfname = tempnam(sys_get_temp_dir(), 'alg');
@@ -65,11 +85,23 @@ class Aligent_Emarsys_Model_HarmonyFTPClient {
         }
     }
 
+    /**
+     * Shortcut to create the remote file $fileName in the configured remote
+     * export directory, with the contents of $fileContents
+     *
+     * @param $fileName
+     * @param $fileContents
+     * @return mixed
+     */
     public function putExportFromString($fileName, $fileContents){
         $this->chdir($this->_exportDir);
         return $this->putFromString($fileName, $fileContents);
     }
 
+    /**
+     * Creates the actual underlying client, based on settings for
+     * FTP or sFTP
+     */
     protected function _createClient(){
         $this->_actualClient = $this->_sFTP ? new Varien_Io_Sftp() : new FtpClient();
     }
